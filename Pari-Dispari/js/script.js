e7d3eb76f6e5e17692833cf3257c955e2bbc8e30
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-12 17:53:20 +0100

    fix : do while cicle

--- Pari-Dispari/js/script.js
+++ Pari-Dispari/js/script.js
@@ -12,18 +12,18 @@ const evenDomElement = document.querySelector('.pari');
 const oddDomElement = document.querySelector('.dispari');
 
 //genero una funzione che inserendo due numeri ne fa la somma e verifica che il risultato sia pari
-function isEven(num1,num2){
+function isEven(num1, num2) {
 
     const sum = num1 + num2;
-    console.log('la somma e`:',sum)
+    console.log('la somma e`:', sum)
     const rest = sum % 2;
     //SE la variabile rest contenente il resto di sum % 2 e' 0 allora sara' pari e 
     //dara' di ritorno il valore booleano true
-    if(rest === 0){
+    if (rest === 0) {
         console.log('e` pari')
         return true
     }//ALTRIMENTI sara' dispari e dara' di ritorno il valore booleano false
-    else{ 
+    else {
         console.log('e` dispari')
         return false
     }
@@ -31,52 +31,64 @@ function isEven(num1,num2){
 
 
 //aggancio le variabili all'event listener per il pari
-evenDomElement.addEventListener('click',function(){
-    do{
-        //richiedo all'utente di inserire un numero da 1 a 5 e la inserisco nella variabile userNumber
-        const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
-        
-        if(userNumber > 5 || userNumber < 1){
-            console.log('inserisci un numero nel range indicato')
-        }else{
-        //stampo in console il numero inserito dall'utente
-        console.log('il tuo numero e`:',userNumber)
+evenDomElement.addEventListener('click', function () {
+    let userNumber;
+    do {
+        if (userNumber !== undefined) {
+            console.log('inserisci un numero nel range indicato');
         }
-    }while(userNumber > 5 && userNumber < 1)
+
+        //richiedo all'utente di inserire un numero da 1 a 5 e la inserisco nella variabile userNumber
+        userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+
+    } while (isNaN(userNumber) || userNumber > 5 || userNumber < 1);
+
+    console.log('il tuo numero e`:',userNumber)
+
+    
     //creo una variabile che genera un numero casuale da 1 a 5 per il pcNumber
     const pcNumber = Math.floor(Math.random() * 5) + 1;
 
     //stampo in console il numero generato per il PC
-    console.log('il numero del PC e`:',pcNumber)
-    
+    console.log('il numero del PC e`:', pcNumber)
+
     //invoco la fuzione isEven creata a monte per sommare le variabili ottenute 
-    const result = isEven(userNumber,pcNumber)
+    const result = isEven(userNumber, pcNumber)
 
     //SE la funzione restituisce true vuol dire che e' pari
-    if(result === true){
+    if (result === true) {
         console.log('hai vinto')
-    //Altrimenti vuol dire che e' dispari
-    }else{
+        //Altrimenti vuol dire che e' dispari
+    } else {
         console.log('ha vinto il pc')
     }
-
 })
 //Stesso procedimento di evenDomElement
-oddDomElement.addEventListener('click',function(){
-    const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+oddDomElement.addEventListener('click', function () {
+    let userNumber;
+    do {
+        if (userNumber !== undefined) {
+            console.log('inserisci un numero nel range indicato');
+        }
+
+        //richiedo all'utente di inserire un numero da 1 a 5 e la inserisco nella variabile userNumber
+        userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+
+    } while (isNaN(userNumber) || userNumber > 5 || userNumber < 1);
+
     console.log('il tuo numero e`:',userNumber)
 
     const pcNumber = Math.floor(Math.random() * 5) + 1;
-    console.log('il numero del PC e`:',pcNumber)
-    
-    const result = isEven(userNumber,pcNumber)
+    console.log('il numero del PC e`:', pcNumber)
+
+    const result = isEven(userNumber, pcNumber)
 
     //con la differenza che:
     //SE la funzione restituisce true, con l'operatore di diversita , vincera` cmq l'utente perche' risultera' false
     //(effettivamente potevo mettere anche result === false)
-    if(result !== true){
+    if (result !== true) {
         console.log('hai vinto')
-    }else{
+    } else {
         console.log('ha vinto il pc')
     }
 
